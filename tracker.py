c07c151f28ca58fea07f267803fdf80db3756442
Sahithi <sahithireddy422@gmail.com>
2026-04-25 08:50:32 +0000

    added deletion of rows and csv file feature

--- tracker.py
+++ tracker.py
@@ -1,40 +1,52 @@
 from logging import log
-from today import today
+from view import today,totals
+from delete import delete_row, del_all
+
 
 def main():
-    print("Study Tracker")
+
+    print("\nStudy Tracker")
     print("1. Log a session")
     print("2. View today's sessions")
     print("3. View totals")
-    print("4. Quit")
+    print("4. Delete a log")
+    print("5. Clear all logs")
+    print("6. Quit")
 
-main()
+    choice=int(input("Enter your choice: "))
 
-choice=int(input("Enter your choice: "))
+    print('\n')
+
+    match choice:
+        case 1:
+            log()
+        case 2:
+            today()
+        case 3:
+            totals()
+        case 4:
+            delete_row()
+        case 5:
+            del_all()
+        case 6:
+            print("Bye!!!")
+            exit(1)
+            
+if __name__=="__main__":
+    while True:
+        main()
 
 
-match choice:
-    case 1:
-        log()
-    case 2:
-        today()
-#     case 3:
-#         # viewing totals
-#     case 4:
-#         # exit
+# how to view totals?
+# date    subject     duration        notes     time
+
+# how to delete a log?
+# ask for the row to delete(simple)
     
 
 
 
-#1. log date 
-    # ->manual
-    # ->automatic(done)
 
-# 2. to view today's sessions
-    # format:-
-        # date:-     time:-
-        # subject:-    hrs_studied:-
-        # notes:- 
 
 
 
