e10a120ef53b959a0968a8ed49a16003888c1fe9
Sahithi <sahithireddy422@gmail.com>
2026-04-25 05:20:53 +0000

    Modified to include time of logging

--- today.py
+++ today.py
@@ -8,9 +8,12 @@ def today():
         fields = ['subject', 'hrs', 'notes', 'dt_timestamp']
         reader=csv.DictReader(file, fieldnames=fields)
 
+        print(f"{'Subject':<15} | {'Hours':<7} | {'Notes':<30} | {'Logging time'}")
+        print('\n')
+
         for row in reader:
 
             row_date=row['dt_timestamp'][:10]
             if row_date==target_date:
-                print(f"{row['subject']:<15} | {row['hrs']} hrs | {row['notes']}")
+                print(f"{row['subject']:<15} | {row['hrs']} hrs | {row['notes']:<30} | {row['dt_timestamp'][12:]}")
 
